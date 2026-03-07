1b966181de1c493f960888165051d8f522ef4ece
Guilherme Araujo <guidrop17@gmail.com>
2026-03-06 22:08:12 -0300

    feat: initialize Angular application with basic structure and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'pt',
+      fallbackLang: 'pt',
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json',
+      }),
+    }),
+  ],
 };
